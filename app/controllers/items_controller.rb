13699b5853011fdc9ecf6166d71b72974077fa04
Dan Muendo <danwalker254254@gmail.com>
2022-11-25 12:46:39 -0500

    assignment complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+    def index
+        item = Item.all
+        render json: item, except: [:created_at, :updated_at, :user_id], include: :user
+    end
 end
